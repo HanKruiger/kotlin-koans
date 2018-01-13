baf5a75b8887f6ef25d244bc242a0e127ef8f8b3
Han Kruiger <hankruiger@gmail.com>
2018-01-13 18:13:07 +0100

    Progress on conventions section

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,17 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int): Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        if (this.year - other.year != 0)
+            return this.year - other.year
+        else if (this.month - other.month != 0)
+            return this.month - other.month
+        return this.dayOfMonth - other.dayOfMonth
+    }
+}
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+// Operator for the "date1..date2" range syntax
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
 
 enum class TimeInterval {
     DAY,
@@ -11,3 +20,30 @@ enum class TimeInterval {
 }
 
 class DateRange(val start: MyDate, val endInclusive: MyDate)
+
+// Operator for the "date in DateRange(date1, date2)" condition syntax
+operator fun DateRange.contains(date: MyDate): Boolean = date >= start && date <= endInclusive
+
+// Operator for the "for date in DateRange(date1, date2)" loop syntax
+operator fun DateRange.iterator() = object: Iterator<MyDate> {
+    var current: MyDate = this@iterator.start
+    override fun next(): MyDate {
+        val result = current
+        current = current.nextDay()
+        return result
+    }
+    override fun hasNext(): Boolean =  current in this@iterator
+}
+// NOTE to self: Use RepeatedTimeInterval
+class MyTimeInterval(val days: Int, val weeks: Int, val years: Int)
+operator fun MyTimeInterval.plus(other: MyTimeInterval): MyTimeInterval {
+    return MyTimeInterval(this.days + other.days, this.weeks + other.weeks, this.years + other.years)
+}
+
+// Operator for adding time intervals to the date
+operator fun MyDate.plus(interval: MyTimeInterval): MyDate {
+    return this
+            .addTimeIntervals(TimeInterval.YEAR, interval.years)
+            .addTimeIntervals(TimeInterval.WEEK, interval.weeks)
+            .addTimeIntervals(TimeInterval.DAY, interval.days)
+}
